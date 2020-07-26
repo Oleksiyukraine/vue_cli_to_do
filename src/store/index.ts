d75228e1e66a152dab1743589634044829cd11c7
Oleksiyukraine <Oleksiyukraine@gmail.com>
2020-07-26 21:10:29 +0300

    Create tasks compleate

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,10 +5,21 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    tasks: [localStorage.getItem('tasks')] || []
   },
   mutations: {
+    createTask (state, payload) {
+      state.tasks.push(payload)
+      localStorage.setItem('tasks', JSON.stringify(state.tasks))
+    }
   },
   actions: {
+    createTask ({ commit }, payload) {
+      commit('createTask', payload)
+    }
+  },
+  getters: {
+    tasks: s => s.tasks
   },
   modules: {
   }
---
+++ src/views/List.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h1>List</h1>
+    <hr>
+    <table v-if="tasks.length">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Title</th>
+          <th>Desk</th>
+          <th>Date</th>
+          <th>Status</th>
+          <th>Open</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(task, index) in tasks"
+          :key="task.id"
+        >
+          <td>{{ index + 1 }}</td>
+          <td>{{ task.title }}</td>
+          <td>{{ task.description }}</td>
+          <td>{{ task.date }}</td>
+          <td>{{ task.status }}</td>
+          <td>
+            <router-link :to="`/task/${task.id}`" tag="button" class="btn btn-small">
+              Open
+            </router-link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <p v-else>
+      No tasks
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  components: {
+  },
+  data: () => ({
+  }),
+  computed: {
+    tasks () {
+      return this.$store.getters.tasks
+    }
+  }
+}
+</script>
